adb4e541e5e22ccbd9393c04b26380b5e2ef04e1
isayani <isayani95@gmail.com>
2022-11-25 16:09:19 -0500

    CTA component and header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './header.css'
+import CTA from './CTA'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+      <h5>Hello I'm</h5>
+      <h1>Insha Sayani</h1>
+      <h5 className='text-light'>Full-Stack Developer</h5>
+      <CTA />
+      </div>
+    </header>
   )
 }
 
