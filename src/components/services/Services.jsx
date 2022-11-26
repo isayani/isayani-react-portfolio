0581bd22b0485b11f184b4fe31b9889f4b00d3aa
isayani <isayani95@gmail.com>
2022-11-26 01:33:07 -0500

    servces jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,90 @@
 import React from 'react'
 import './services.css'
+import {FiCheck} from 'react-icons/fi'
 
 function Services() {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Can Do</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className='service'>
+          <div className="service__head">
+            <h3> UI/UX Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* START WEB DEVELOPMENT */}
+        <article className='service'>
+          <div className="service__head">
+            <h3> Web Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* START OF CONTENT CREATION */}
+        <article className='service'>
+          <div className="service__head">
+            <h3> Content Creation</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><FiCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
