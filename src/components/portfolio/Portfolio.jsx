779480c135ea6368d4d319d75c06cdf24d02878b
isayani <isayani95@gmail.com>
2022-11-26 21:45:23 -0500

    mapped throug data

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,75 +1,95 @@
-import React from 'react';
-import './portfolio.css'
-import cover1 from '../../assets/portfolio1.jpg'
-import cover2 from '../../assets/portfolio2.jpg'
-import cover3 from '../../assets/portfolio3.jpg'
-import cover4 from '../../assets/portfolio4.jpg'
-import cover5 from '../../assets/portfolio5.png'
-import cover6 from '../../assets/portfolio6.jpg'
+import React from "react";
+import "./portfolio.css";
+import cover1 from "../../assets/portfolio1.jpg";
+import cover2 from "../../assets/portfolio2.jpg";
+import cover3 from "../../assets/portfolio3.jpg";
+import cover4 from "../../assets/portfolio4.jpg";
+import cover5 from "../../assets/portfolio5.png";
+import cover6 from "../../assets/portfolio6.jpg";
+
+// SAMPLE IMAGES NOT FOR PRODUCTION
+
+const data = [
+  {
+    id: 1,
+    image: cover1,
+    title: "Project 1",
+    github: "https://github.com/isayani",
+    demo: "https://github.com/isayani",
+  },
+  {
+    id: 2,
+    image: cover2,
+    title: "Project 2",
+    github: "https://github.com/isayani",
+    demo: "https://github.com/isayani",
+  },
+  {
+    id: 3,
+    image: cover3,
+    title: "Project 3",
+    github: "https://github.com/isayani",
+    demo: "https://github.com/isayani",
+  },
+  {
+    id: 4,
+    image: cover4,
+    title: "Project 4",
+    github: "https://github.com/isayani",
+    demo: "https://github.com/isayani",
+  },
+  {
+    id: 5,
+    image: cover5,
+    title: "Project 5",
+    github: "https://github.com/isayani",
+    demo: "https://github.com/isayani",
+  },
+  {
+    id: 6,
+    image: cover6,
+    title: "Project 6",
+    github: "https://github.com/isayani",
+    demo: "https://github.com/isayani",
+  },
+];
 
 function Portfolio() {
   return (
-    <section id='portfolio'>
-    <h5>My Recent Work</h5>
-    <h2>Portfolio</h2>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-    <div className="container portfolio__container">
-      <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={cover1} alt="Project Title 1" />
-        </div>
-        <h3>Sample Project Title</h3>
-        <a href="https://github.com/isayani" className='btn' target='_blank'>GitHub Repo</a>
-        <a href="https://github.com/isayani" className='btn btn-primary' target='_blank'>Live Demo</a>
-      </article>
-
-      <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={cover1} alt="Project Title 1" />
-        </div>
-        <h3>Sample Project Title</h3>
-        <a href="https://github.com/isayani" className='btn' target='_blank'>GitHub Repo</a>
-        <a href="https://github.com/isayani" className='btn btn-primary' target='_blank'>Live Demo</a>
-      </article>
-
-      <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={cover1} alt="Project Title 1" />
-        </div>
-        <h3>Sample Project Title</h3>
-        <a href="https://github.com/isayani" className='btn' target='_blank'>GitHub Repo</a>
-        <a href="https://github.com/isayani" className='btn btn-primary' target='_blank'>Live Demo</a>
-      </article>
-
-      <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={cover1} alt="Project Title 1" />
-        </div>
-        <h3>Sample Project Title</h3>
-        <a href="https://github.com/isayani" className='btn' target='_blank'>GitHub Repo</a>
-        <a href="https://github.com/isayani" className='btn btn-primary' target='_blank'>Live Demo</a>
-      </article>
-
-      <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={cover1} alt="Project Title 1" />
-        </div>
-        <h3>Sample Project Title</h3>
-        <a href="https://github.com/isayani" className='btn' target='_blank'>GitHub Repo</a>
-        <a href="https://github.com/isayani" className='btn btn-primary' target='_blank'>Live Demo</a>
-      </article>
-
-      <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={cover1} alt="Project Title 1" />
-        </div>
-        <h3>Sample Project Title</h3>
-        <a href="https://github.com/isayani" className='btn' target='_blank'>GitHub Repo</a>
-        <a href="https://github.com/isayani" className='btn btn-primary' target='_blank'>Live Demo</a>
-      </article>
-    </div>
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                >
+                  GitHub Repo
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
     </section>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
