95d9e2f6b834c333af98932707123c6d7d0fd146
isayani <isayani95@gmail.com>
2022-12-08 22:43:18 -0500

    fixed overlap bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import './header.css'
-import CTA from './CTA'
-import Me from '../../assets/IMG_8775.png'
-import HeaderSocials from './HeaderSocials';
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import Me from "../../assets/IMG_8775.png";
+import HeaderSocials from "./HeaderSocials";
 
 function Header() {
   return (
-    <header id='header'>
-      <div className="container header__container">
-      <h5>Hello I'm</h5>
-      <h1>Insha Sayani</h1>
-      <h5 className='text-light'>Full-Stack Developer</h5>
-      <CTA />
+    <>
+      <header id="header">
+        <div className="container header__container">
+          <h5>Hello I'm</h5>
+          <h1>Insha Sayani</h1>
+          <h5 className="text-light">Full-Stack Developer</h5>
+          <CTA />
+          <div>
+            <div className="me">
+              <img src={Me} alt="image of Insha Sayani" />
+            </div>
+          </div>
+        </div>
+      </header>
       <HeaderSocials />
-      <div className="me">
-        <img src={Me} alt='image of Insha Sayani'/>
-      </div>
-
-      <a href="#footer" className='scroll__up'> Scroll Down </a>
-      </div>
-    </header>
-  )
+      <a href="#footer" className="scroll__down">
+        {" "}
+        Scroll Down{" "}
+      </a>
+    </>
+  );
 }
 
-export default Header
+export default Header;
